f181d38262a642d1c5cc537516df8a9e9652b7e0
Vasilii31 <franco.olivier@yahoo.fr>
2025-08-05 14:24:18 +0100

    ajout db

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const express = require('express');
 const path = require('path');
 const apiRoutes = require('./Back/api');
 const app = express();
+const mongoose = require('mongoose');
 
+const mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017/mydatabase';
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
@@ -15,7 +17,36 @@ app.get('/', (req, res) => {
 // Serve static files from "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connecté !'))
+  .catch(err => console.error('Erreur de connexion MongoDB:', err));
 
+
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+});
+const User = mongoose.model('User', UserSchema);
+
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find(); // récupère tous les users en base
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.post('/users', async (req, res) => {
+  const user = new User(req.body); // on crée avec les données envoyées
+  try {
+    await user.save();
+    res.status(201).json(user);
+    console.log("user créé");
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 
 
 app.listen(PORT, () => {

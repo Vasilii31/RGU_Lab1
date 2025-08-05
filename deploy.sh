70e3275b28bb96a1f20001fbbcd6ff916ab38128
Vasilii31 <franco.olivier@yahoo.fr>
2025-08-05 15:29:44 +0100

    final deployment script

--- deploy.sh
+++ deploy.sh
@@ -8,28 +8,29 @@ if [ -z "$1" ]; then
 fi
 
 TAG=$1
-REPO_DIR=~/lab2
-PROD_DIR=$REPO_DIR/prod
+PROD_DIR=~/lab2/prod
 COMPOSE_FILE=$PROD_DIR/docker-compose.yml
-IMAGE_NAME="mon-app" # Remplace par le nom de ton image si différent
+IMAGE_NAME="vasilii31/lab2" # Remplace par le nom de ton image si différent
 
 echo "1. Pull du dépôt Git..."
-cd $REPO_DIR || { echo "Erreur : Répertoire $REPO_DIR introuvable"; exit 1; }
 git pull || { echo "Erreur : Échec du git pull"; exit 1; }
 
 echo "2. Build de l'image avec le tag $TAG..."
-docker build -t $IMAGE_NAME:$TAG . || { echo "Erreur : Build échoué"; exit 1; }
+sudo docker build -t $IMAGE_NAME:$TAG . || { echo "Erreur : Build échoué"; exit 1; }
 
-echo "3. Passage dans le dossier $PROD_DIR..."
+echo "3. Push de l'image avec le tag $TAG..."
+sudo docker push $IMAGE_NAME:$TAG || { echo "Erreur : Push echoue"; exit 1;}
+
+echo "4. Passage dans le dossier $PROD_DIR..."
 cd $PROD_DIR || { echo "Erreur : Répertoire $PROD_DIR introuvable"; exit 1; }
 
-echo "4. Arrêt des conteneurs existants..."
-docker compose down
+echo "5. Arrêt des conteneurs existants..."
+sudo docker compose down
 
 echo "5. Mise à jour du tag dans le docker-compose.yml..."
-sed -i.bak -E "s|image: $IMAGE_NAME:.*|image: $IMAGE_NAME:$TAG|" "$COMPOSE_FILE"
+sudo sed -i.bak -E "s|image: $IMAGE_NAME:.*|image: $IMAGE_NAME:$TAG|" "$COMPOSE_FILE"
 
 echo "6. Redémarrage des conteneurs avec le nouveau tag..."
-docker compose up -d
+sudo docker compose up -d
 
 echo "Déploiement terminé avec l'image : $IMAGE_NAME:$TAG"

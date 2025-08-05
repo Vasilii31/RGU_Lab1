dd0dcb00a7b73b4a895dff5d99f0a814fed7c3a5
Vasilii31 <franco.olivier@yahoo.fr>
2025-08-05 09:48:27 +0100

    ajout docker components

--- Back/api.js
+++ Back/api.js
@@ -3,11 +3,16 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/calculate', (req, res) => {
-  const { salary, days } = req.body;
 
+  
+  const { salary, days } = req.body;
+  console.log("salary : " + salary);  
+  console.log("days : " + days); 
   const dailyRate = parseFloat(salary) / 365;
   const finalPrice = Math.round(dailyRate * parseFloat(days));
 
+  console.log("dailyRate : " + dailyRate); 
+  console.log("finalPrice : " + finalPrice); 
   res.json({ finalPrice });
 });
 
--- server.js
+++ server.js
@@ -1,21 +1,19 @@
 // server.js
 const express = require('express');
 const path = require('path');
-const apiRoutes = require('./api');
+const apiRoutes = require('./Back/api');
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-// Serve static files from "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-
 app.use('/api', apiRoutes);
 // Default route
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'myWebPage.html'));
 });
-
+// Serve static files from "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 

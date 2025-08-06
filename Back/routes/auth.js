1d5581023eaa98805a2a49eef9d4ee23270fc18a
Vasilii31 <franco.olivier@yahoo.fr>
2025-08-06 14:22:43 +0100

    correction token

--- Back/routes/auth.js
+++ Back/routes/auth.js
@@ -31,7 +31,7 @@ router.post('/login', async (req, res) => {
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ message: 'Mot de passe incorrect' });
 
-    const token = jwt.sign({ id: user._id, email: user.email }, process.env.JWT_SECRET, {
+    const token = jwt.sign({ id: user._id, email: user.email }, "TestChaineSecrete", {
       expiresIn: '1h',
     });
 
